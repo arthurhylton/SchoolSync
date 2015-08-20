affb70a912f2bf0b1fa2104636cbcc8d4b3977f7
Arthur Hylton <arthur.hylton@gmail.com>
2015-08-20 08:48:06 -0500

    Basic changes between 2 dbf files are identified and logged

---
+++ SchoolSync/Classes/ExtensionMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSync
+{
+    public static class ExtensionMethods
+    {
+        public static string Implode(this School school)
+        {
+            PropertyInfo[] _PropertyInfos = null;
+            if(_PropertyInfos == null)
+                _PropertyInfos = school.GetType().GetProperties();
+
+            var sb = new StringBuilder();
+
+            foreach (var info in _PropertyInfos)
+            {
+                var value = info.GetValue(school, null) ?? "";
+                sb.AppendLine(value.ToString()+"~");
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ SchoolSync/Classes/School.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSync
+{
+    public class School
+    {
+        public string SchoolCode { get; set; }
+        public string Name { get; set; }
+        public int RegionNumber { get; set; }
+        public int RegionName { get; set; }
+    }
+}
