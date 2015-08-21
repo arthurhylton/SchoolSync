6bb62e16831fc0d4bcc859c8afe5b60b55003e3d
Arthur Hylton <arthur.hylton@gmail.com>
2015-08-21 16:17:31 -0500

    Posting changes to remote web url

--- SchoolSync/MainWindow.xaml.cs
+++ SchoolSync/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
 
 namespace SchoolSync
 {
@@ -25,6 +28,36 @@ namespace SchoolSync
         {
             InitializeComponent();
             this.DataContext = vm;
+            PostSchoolsToServer(vm.Changes.ToList());
+        }
+
+        private void PostSchoolsToServer(List<School> schools)
+        {
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(Properties.Settings.Default.RemoteDatabaseWebServiceURL);
+            httpWebRequest.ContentType = "text/json";
+            httpWebRequest.Method = "POST";
+
+            using(var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string json = JsonConvert.SerializeObject(schools);
+
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using(var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+                global::System.Windows.MessageBox.Show(result);
+            }
+            //Dictionary<string, string> data = new Dictionary<string, string>();
+            //int index = 0;
+            //foreach (var school in schools)
+            //{
+            //    data.Add()
+            //}
         }
     }
 }
